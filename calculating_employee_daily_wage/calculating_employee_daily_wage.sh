b7b968b47bced622705207db16442c9de19946ea
saurabhrajankar <saurabhrajankar777@gmail.com>
2022-09-01 11:13:52 +0530

    added

---
+++ calculating_employee_daily_wage/calculating_employee_daily_wage.sh
@@ -0,0 +1,15 @@
+echo "Calculating employee daily wage"
+
+isPresent=1
+randomCheck=$((RANDOM%2))
+
+if [ $randomCheck -eq $isPresent ]
+then
+	empRatePerHr=20;
+	empHrs=8;
+	salary=$(( $empRatePerHr*$empHrs ))
+else
+	salary=0;
+fi
+
+echo "Daily Wage of an Employee is " $salary
